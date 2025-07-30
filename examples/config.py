1f59ea59befabe9385f3108df97613d09881a47d
chrisdoman <cdoman+github@cadosecurity.com>
2025-07-30 15:44:29 +0100

    Update config.py to https

--- examples/config.py
+++ examples/config.py
@@ -2,8 +2,8 @@
 #
 # Please edit the variables here before running any of the examples.
 
-PLATFORM_IP = "localhost:5000"
-API_URL = f"http://{PLATFORM_IP}/api/v3"  # Don't edit this value
+PLATFORM_IP = "example.com"
+API_URL = f"https://{PLATFORM_IP}/api/v3"  # Don't edit this value
 # The Secret Key can be found in the platform under Settings -> API Keys
 API_KEY = ""
 TEST_PROJECT_ID = 1  # project that already exists in the platform with test data
